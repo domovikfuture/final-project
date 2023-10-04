4ef8986720fa8d5e1e7f703f1b55ca862caea2bc
stanislavdrozd <domovik.future@gmail.com>
2023-10-04 11:44:10 +0300

    Changed server file

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (!dataAccessLayer) {
   dataAccessLayer.connect();
 }
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend", "/build")));
 
 app.get("*", (req, res) =>
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
